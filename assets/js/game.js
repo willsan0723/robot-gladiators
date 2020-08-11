e2d42947c6047502c5682622fd9b06ac05dc22d8
willsan0723 <william.santee@gmail.com>
2020-08-10 18:55:27 -0600

    started coding javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+  //fight();
