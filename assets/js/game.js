f06573b22ca021a29f256a4420ac6bd0d0a409a3
willsan0723 <william.santee@gmail.com>
2020-08-11 17:43:58 -0600

    now fights all robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,9 +13,8 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 var fight = function(enemyName) {
-  //  fight funtion statements
-  // Alert users that they are starting the round
-//  window.alert("Welcome to Robot Gladiators!");
+  while(enemyHealth > 0) {
+  //  fight funtion statements  
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   console.log(promptFight);
 
@@ -65,6 +64,10 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   window.alert("You need to pick a valid option. Try again!");
 }
 };
+  }
+
 for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth= 50;
+  fight(pickedEnemyName);
 }
