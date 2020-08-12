9b26c8ae6bd812fe1dc602f7e4ad69bcfe3a5296
willsan0723 <william.santee@gmail.com>
2020-08-11 18:49:09 -0600

    added game over condition

--- assets/js/game.js
+++ assets/js/game.js
@@ -59,7 +59,7 @@ var fight = function(enemyName) {
 
     // check player's health
     if (playerHealth <= 0) {
-      window.alert(playerName + ' has died!');
+      window.alert(playerName + ' has died!');      
       // leave while() loop if player is dead
       break;
     } else {
@@ -69,7 +69,24 @@ var fight = function(enemyName) {
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth= 50;
-  fight(pickedEnemyName);
+  if (playerHealth > 0) {
+    // let user know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+    // pick new enemy to fight based on the index of the enemyNames array
+    var pickedEnemyName = enemyNames[i];
+
+    // reset enemyHealth before starting new fight
+    enemyHealth = 50;
+
+    // use debugger to pause script from running and check what's going on at that moment in the code
+    // debugger;
+
+    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+    fight(pickedEnemyName);
+  }
+  else {
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+  }
 }
